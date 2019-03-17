40c47571e052d397f88694c139044033dee0ec52
J. Singleton <jennifer.singleton@gmail.com>
2019-03-17 14:11:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,8 +17,7 @@ class Student
       FROM students
     SQL
 
-    rows = DB[:conn].execute(sql)
-    rows.map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
